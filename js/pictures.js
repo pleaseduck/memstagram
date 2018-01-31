f3a302b5d6f04c972622d43d0a5f74c9fa306faa
pleaseduck <pikov.artem@icloud.com>
2018-01-31 12:31:15 +0300

    Начал делать первое дз

---
+++ js/pictures.js
@@ -0,0 +1,54 @@
+var photoLinks = ['photos/1.jpg','photos/2.jpg','photos/3.jpg','photos/4.jpg','photos/5.jpg','photos/6.jpg','photos/7.jpg','photos/8.jpg','photos/9.jpg','photos/10.jpg','photos/11.jpg','photos/12.jpg','photos/13.jpg','photos/14.jpg','photos/15.jpg','photos/16.jpg','photos/17.jpg','photos/18.jpg','photos/19.jpg','photos/20.jpg','photos/21.jpg','photos/22.jpg','photos/23.jpg','photos/24.jpg', 'photos/25.jpg']
+var photoLikes = [];
+var photoComments = ['Всё отлично!',
+'В целом всё неплохо. Но не всё',
+'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это епрофессионально',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше',
+'Лица у людей на фотке перекошены, как будто их избивают.  Как вообще можно было поймать такой неудачный момент?!'];
+  var photos = [];
+
+for (var i = 0; i < 25; i++) {
+  photoLikes[i]= randomInteger(15,200)
+}
+
+for (var i = 0; i < 25; i++) {
+  var photoDescription = {};
+  photoDescription[i] = {
+    'url':  photoLinks[randomNumber(photoLinks)],
+    'likes': photoLikes[randomNumber(photoLikes)],
+    'comments': photoComments[randomNumber(photoComments)]
+  };
+  photos[i] = photoDescription;
+}
+console.log(photos)
+
+for (var i = 0; i < 4; i++) {
+  var pictureTemplate = document.querySelector('#picture-template');
+  pictureTemplate.content.querySelector('.picture-comments').innerHTML = photos[randomNumber(photos)].comments;
+  pictureTemplate.content.querySelector('.picture-likes').innerHTML = photos[randomNumber(photos)].likes;
+  pictureTemplate.content.querySelector('img').src = photos[randomNumber(photos)].url;
+  var picturesContainer = document.querySelector('.pictures');
+  picturesContainer.appendChild(pictureTemplate.content.cloneNode(true));
+}
+
+function randomInteger(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+  }
+  function randomNumber(array) {
+  return Math.floor(Math.random()*array.length);
+}
+/*
+for (var i = 0; i < 4; i++) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template');
+  similarWizardTemplate.content.querySelector('.setup-similar-label').innerHTML =  wizardСharacteristics.name[randomNumber(wizardСharacteristics.name)] +
+  wizardСharacteristics.surname[randomNumber(wizardСharacteristics.surname)];
+  similarWizardTemplate.content.querySelector('.wizard-coat').style.fill = wizardСharacteristics.coat[randomNumber(wizardСharacteristics.coat)];
+  similarWizardTemplate.content.querySelector('.wizard-eyes').style.fill = wizardСharacteristics.eyesColor[randomNumber(wizardСharacteristics.eyesColor)];
+
+  wizardsList.appendChild(similarWizardTemplate.content.cloneNode(true));
+  console.log(similarWizardTemplate.content.querySelector('.setup-similar-label'))
+}
+*/
